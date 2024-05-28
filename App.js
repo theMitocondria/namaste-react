607de48afeddc241ca9e91df058223e3673d9078
Dhruv Mehta <dhruvmehta382@gmail.com>
2024-05-28 12:38:47 +0530

    Ch-02 completed

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
+//after installation
+import React from "react";
+import ReactDOM from "react-dom/client";
 //creating any element , {} => curly bracket me attributes jaaege aapke tags ke example , src , id , class , type
 const heading = React.createElement("h1", { id: "red" }, "Hi I am from Reactjs")
 //creating a root is compulsory and is done by dom 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//adding heading
+
 const nest_div = React.createElement("div", { id: "parent" },
     React.createElement("div", { id: "child" },
-        React.createElement("h1", {}, "I am rendered by nesting")
+        [
+            React.createElement("h1", {}, "I  rendered by nesting"),
+            React.createElement("h2", {}, "I am rendered by nesting2")
+        ]
     )
 )
 root.render(nest_div);
-console.log(nest_div);
+// console.log(nest_div);s
---
+++ README.md
@@ -0,0 +1,12 @@
+# Namaste React
+
+# Parcel
+- Localhost sercer bnaya
+- local build bnaya
+- image ko optimize krega
+- caching kreag(cache parcel me dekho) for faster reload
+- automatic reloading throuh HMR = Hot Module Reload
+- fuinal production build ka size km kreaga
+- compress , optimization sab krta h (Beast hora h humari jaan ko)
+- differential Bundler : Har browser ke version ke hisaab se sabke alag bundle bnaega
+- Error Handling
